011415db919abbff8a8519c5256701ffd975e9b5
Bruna Aparecida <Bruna@MACECOC02V57EVH.local>
2022-06-13 08:40:45 -0300

    meu primeiro api express

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 // socorro deeeeeeus
+// socorro deus
+
+const express = require ('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send({message: 'Ola mundo'});
+})
+
+app.listen(3001, () => {
+    console.log('Api rodando na porta 3001');
+})
+
 

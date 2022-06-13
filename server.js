58e7cb4b331a6ee64651ee8784bcf064662f0588
BarbosaBruna <91513067+BarbosaBruna@users.noreply.github.com>
2022-06-13 08:55:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     res.status(200).send({message: 'Ola mundo'});
 })
 
+app.get('/ola', (req, res) => {
+    res.status(200).send({message: 'Oi sou Ola'});
+})
+
+
 app.listen(3001, () => {
     console.log('Api rodando na porta 3001');
 })
